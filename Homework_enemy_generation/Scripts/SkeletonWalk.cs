37661c0c29b9cf02fa255f8f7f822d5bd6e57400
Alexandr <hydral1sk89@gmail.com>
2023-08-12 19:29:49 +0300

    Homework_enemy_spwans
    Complete Homework_enemy_spawns v.1

---
+++ Homework_enemy_generation/Scripts/SkeletonWalk.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonWalk : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _target;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody2D.velocity = _target.position * _speed;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+}
---
+++ Homework_enemy_generation/Scripts/SpawnController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _spawns;
+    [SerializeField] private List<Transform> _directions;
+    [SerializeField] private SkeletonWalk _unit;
+    [SerializeField] private float _spawnDelay;
+
+    private Coroutine _createUnit;
+
+    private void Start()
+    {
+        _createUnit = StartCoroutine(CreateMonster(_spawnDelay));
+    }
+
+    IEnumerator CreateMonster(float delay)
+    {
+        int countSkeletons = 100;
+
+        for (int i = 0; i < countSkeletons; i++)
+        {
+            var wait = new WaitForSecondsRealtime(delay);
+
+            yield return wait;
+
+            int randomSpawner = Random.RandomRange(0, _spawns.Count);
+            int randomDirection = Random.RandomRange(0, _directions.Count);
+
+            Vector3 position = _spawns[randomSpawner].position;
+
+            SkeletonWalk skeleton = Instantiate(_unit, position, Quaternion.identity);
+            skeleton.SetTarget(_directions[randomDirection]);
+        }
+    }
+}
